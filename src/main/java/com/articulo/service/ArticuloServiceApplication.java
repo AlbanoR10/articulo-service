5c061b059ed427a021290375d2e04e06a3942c7c
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-09-15 11:41:22 -0700

    Creacion de entidad Articulo

--- src/main/java/com/articulo/service/ArticuloServiceApplication.java
+++ src/main/java/com/articulo/service/ArticuloServiceApplication.java
@@ -1,4 +1,4 @@
-package articuloservice;
+package com.articulo.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/articulo/service/entidad/Articulo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.articulo.service.entidad;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "articulo")
+public class Articulo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "unidad_medida", nullable = false)
+    private String unidadMedida;
+
+    @Column(name = "clave", nullable = false)
+    private String clave;
+
+    @Column(name = "precio", nullable = false)
+    private double precio;
+}
