4daa1ccc5cadfa006b1aa0d794a75f187d8eb58f
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-09-15 13:46:59 -0700

    Agregar metodos faltantes a ArticuloController

--- src/main/java/com/articulo/service/controlador/ArticuloController.java
+++ src/main/java/com/articulo/service/controlador/ArticuloController.java
@@ -27,14 +27,16 @@ public class ArticuloController {
     public ResponseEntity<Articulo> obtenerArticulo(@PathVariable("id") int id) {
         Articulo articulo = articuloModelo.getArticuloById(id);
         if (articulo == null) {
+            publisher.send("No se leyo el articulo con id " + id);
             return ResponseEntity.notFound().build();
         }
+        publisher.send("Se leyo el articulo con id " + id);
         return ResponseEntity.ok(articulo);
     }
 
     @PostMapping
-    public ResponseEntity<Articulo> guardarArticulo(@RequestBody Articulo bitacora) {
-        Articulo nuevoArticulo = articuloModelo.save(bitacora);
+    public ResponseEntity<Articulo> guardarArticulo(@RequestBody Articulo articulo) {
+        Articulo nuevoArticulo = articuloModelo.save(articulo);
         if (nuevoArticulo == null) {
             publisher.send("No se guardo el articulo");
             return ResponseEntity.unprocessableEntity().build();
@@ -43,12 +45,48 @@ public class ArticuloController {
         return ResponseEntity.ok(nuevoArticulo);
     }
 
+    @PostMapping("/actualizar")
+    public ResponseEntity<Articulo> actualizarArticulo(@RequestBody Articulo articulo) {
+        Articulo nuevoArticulo = articuloModelo.actualizar(articulo);
+        if (nuevoArticulo == null) {
+            publisher.send("No se actualizo el articulo con id: " + articulo.getId());
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        publisher.send("Se actualizo el articulo con id: " + nuevoArticulo.getId());
+        return ResponseEntity.ok(nuevoArticulo);
+    }
+
     @GetMapping
     public ResponseEntity<List<Articulo>> listarArticulo() {
         List<Articulo> articulos = articuloModelo.getAll();
         if (articulos.isEmpty()) {
+            publisher.send("No hay articulos para listar");
             return ResponseEntity.noContent().build();
         }
+        publisher.send("Se listaron todos los articulos");
         return ResponseEntity.ok(articulos);
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<String> informacionArticulos() {
+        
+        publisher.send("Se pidió informacion");
+        String info = "Campo nombre: Texto de entre 1 y 30 caracteres\n"+
+                        "Campo unidadMedida: 1=Pieza, 2=Kilogramo, 3=Pulgada, 4=Litro\n"+
+                        "Campo clave: Texto de entre 3 y 30 caracteres\n"+
+                        "Campo precio: Numero decimal";
+
+        return ResponseEntity.ok(info);
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable("id") int id) {
+        int respuesta = articuloModelo.eliminar(id);
+        if (respuesta==0) {
+            publisher.send("No existia articulo con id " + id);
+            return "No existia articulo con id " + id;
+        }
+        publisher.send("Se elimino articulo con id " + id);
+        return "Se elimino articulo con id " + id;
     }
 }
