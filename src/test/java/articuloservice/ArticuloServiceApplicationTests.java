3dd4a5c3d4c9e27e2c7a9d739275db2d0a16dbf4
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-09-15 10:45:59 -0700

    Inicio del proyecto articulo-service

---
+++ src/test/java/articuloservice/ArticuloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package articuloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticuloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
