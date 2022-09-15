4b37654deb1d3b013fbd4fe177ddcba016e2a325
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-09-15 13:02:04 -0700

    Agregar codigo para enviar mensajes a RabbitMQ

--- src/main/java/com/articulo/service/controlador/ArticuloController.java
+++ src/main/java/com/articulo/service/controlador/ArticuloController.java
@@ -2,6 +2,7 @@ package com.articulo.service.controlador;
 
 import com.articulo.service.entidad.Articulo;
 import com.articulo.service.modelo.ArticuloModelo;
+import com.articulo.service.publisher.Publisher;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,9 @@ public class ArticuloController {
     @Autowired
     private ArticuloModelo articuloModelo;
 
+    @Autowired
+    private Publisher publisher;
+
     @GetMapping("/{id}")
     public ResponseEntity<Articulo> obtenerArticulo(@PathVariable("id") int id) {
         Articulo articulo = articuloModelo.getArticuloById(id);
@@ -32,15 +36,17 @@ public class ArticuloController {
     public ResponseEntity<Articulo> guardarArticulo(@RequestBody Articulo bitacora) {
         Articulo nuevoArticulo = articuloModelo.save(bitacora);
         if (nuevoArticulo == null) {
+            publisher.send("No se guardo el articulo");
             return ResponseEntity.unprocessableEntity().build();
         }
+        publisher.send("Se guardo articulo con id: " + nuevoArticulo.getId());
         return ResponseEntity.ok(nuevoArticulo);
     }
 
     @GetMapping
-    public ResponseEntity<List<Articulo>> listarArticulo(){
+    public ResponseEntity<List<Articulo>> listarArticulo() {
         List<Articulo> articulos = articuloModelo.getAll();
-        if(articulos.isEmpty()){
+        if (articulos.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(articulos);
